450958812e6e49b0bd5c2c342f52c098bd384400
Chetan Malviya <malviyachetan02@gmail.com>
2023-04-05 16:26:24 +0530

    fav icon

--- src/view/PaymentHistory/index.js
+++ src/view/PaymentHistory/index.js
@@ -1,8 +1,13 @@
+import CustomBreadcrumb from "../../components/CustomBreadcrumb";
 
 
 const PaymentHistory = () => {
+  const itemslist=[{ label: 'Payment History', url: '/paymenthistory'  }, ];
   return (
-    <div className="w-11 pt-3 m-auto">PaymentHistory</div>
+    
+    <div className="w-11 pt-3 m-auto">
+      <CustomBreadcrumb itemslist={itemslist}/>
+      </div>
   )
 }
 
